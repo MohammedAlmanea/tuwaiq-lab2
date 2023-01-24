f11938fbd87fd1be4c299af5c8930983c7dc82f3
MohammedAlmanea <MohammedAlmanea@outlook.com>
2023-01-24 19:20:59 +0300

    Added dist to .gitignore

---
+++ src/app.ts
@@ -0,0 +1,29 @@
+import express, { Request, Response } from 'express';
+
+const app = express();
+const port = 5500;
+
+app.use(
+  (req, res, next) => {
+    console.log('middleware1');
+    next();
+  },
+  (req, res, next) => {
+    console.log('middleware2');
+    next();
+  }
+);
+
+const middle = (req: Request, res: Response, next: Function): void => {
+  console.log('this is from middleware3');
+  next();
+};
+
+// app.use(middle)
+app.get('/hey', middle, (req, res) => {
+  res.send('Hey from Express!');
+});
+
+app.listen(port, () => {
+  console.log(`listening on http://localhost/${port}`);
+});
